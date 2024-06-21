f4a1897fe64a0b025ab828aeb9fdad1a3f6dadc3
kostak94 <slavakostak2010@gmail.com>
2024-06-21 08:21:41 +0200

    refactored user moongose schema, changed package json

--- model/User.js
+++ model/User.js
@@ -14,6 +14,7 @@ const userSchema = new Schema(
       unique: true,
       match: emailRegexp,
     },
+    avatarURL: String,
     subscription: {
       type: String,
       enum: ["starter", "pro", "business"],
